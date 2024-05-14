3d8f3646effc8fd2027ca3111356db30546ea9ec
Vasquez <cvasquez857@gmail.com>
2024-05-13 21:33:11 -0600

    creacion de alertas

--- controladores/clientes/guardar.php
+++ controladores/clientes/guardar.php
@@ -1,16 +1,43 @@
 <?php
-
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+require '../../modelos/cliente.php';
 $mensaje = '';
-    $nit =  $_POST['cli_nit'];
-    // VALIDAR INFORMACION
-    $_POST['cli_nombre'] = htmlspecialchars( $_POST['cli_nombre']);
-    $_POST['cli_apellido'] = htmlspecialchars( $_POST['cli_apellido']);
-    $_POST['cli_precio'] = filter_var( $nit , FILTER_VALIDATE_INT) ;
+$nit = $_POST['cli_nit'];
+// VALIDAR INFORMACION
+$_POST['cli_nombre'] = htmlspecialchars($_POST['cli_nombre']);
+$_POST['cli_apellido'] = htmlspecialchars($_POST['cli_apellido']);
+$_POST['cli_precio'] = filter_var($nit, FILTER_VALIDATE_INT);
 
-if($_POST['cli_nombre'] == '' || $_POST['cli_apellido'] == '' ||  $_POST['cli_nit'] < 0 ){
+if ($_POST['cli_nombre'] == '' || $_POST['cli_apellido'] == '' || $_POST['cli_nit'] < 0) {
     // ALERTA PARA VALIDAR DATOS
     $resultado = [
         'mensaje' => 'DEBE VALIDAR LOS DATOS',
         'codigo' => 2
     ];
-}else{
+} else {
+    try {
+        // REALIZAR CONSULTA
+        $NuevoCliente = new Cliente($_POST);
+        $guardar = $NuevoCliente->guardar();
+        $resultado = [
+            'mensaje' => 'PRODUCTO INSERTADO CORRECTAMENTE',
+            'codigo' => 1
+        ];
+
+    } catch (PDOException $pe) {
+        $resultado = [
+            'mensaje' => 'OCURRIO UN ERROR INSERTANDO A LA BD',
+            'detalle' => $pe->getMessage(),
+            'codigo' => 0
+        ];
+    } catch (Exception $e) {
+        $resultado = [
+            'mensaje' => 'OCURRIO UN ERROR EN LA EJECUCIÓN',
+            'detalle' => $e->getMessage(),
+            'codigo' => 0
+        ];
+    }
+
+}
