b7d33528abfb9107bd4ef9719e1a69d17faf7343
Vasquez <cvasquez857@gmail.com>
2024-05-13 21:23:31 -0600

    creacion de la funcion ejecutar y funcion servir

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,16 +1,18 @@
 <?php
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-abstract class Conexion{
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+abstract class Conexion
+{
 
     protected static $conexion = null;
 
-    protected static function conectar() : PDO{
+    protected static function conectar(): PDO
+    {
         try {
-            self::$conexion = new PDO ("informix:host=192.168.73.71; service=1526; database=curso_656751; server=MV015_tcp; protocol=onsoctcp", "656751", "656751");
-            self:: $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $e) {
+            self::$conexion = new PDO("informix:host=192.168.73.71; service=1526; database=curso_656751; server=MV015_tcp; protocol=onsoctcp", "656751", "656751");
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
             echo "No hay conexion ala Base de Datoss <br>";
             echo $e->getMessage();
             self::$conexion = null;
@@ -18,5 +20,39 @@ abstract class Conexion{
             exit;
         }
         return self::$conexion;
+    }
+
+    // METODO PARA EJECUTAR SENTENCIAS SQL
+
+    public function ejecutar($sql)
+    {
+        $conexion = self::conectar();
+        $sentencia = $conexion->prepare($sql);
+        $resultado = $sentencia->execute();
+        $idInsertado = $conexion->lastInsertId();
+        self::$conexion = null;
+
+        return [
+            "resultado" => $resultado,
+            "id" => $idInsertado
+        ];
+
+    }
+
+    // METODO PARA CONSULTAR INFORMACION
+    public function servir($sql)
+    {
+        $conexion = self::conectar();
+        $sentencia = $conexion->prepare($sql);
+        $sentencia->execute();
+        $data = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+        $datos = [];
+        foreach ($data as $k => $v) {
+            $datos[] = array_change_key_case($v, CASE_LOWER);
+        }
+        self::$conexion = null;
+
+        return $datos;
+
     }
 }
