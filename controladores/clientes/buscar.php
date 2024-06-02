cb75175df18ab8faa8e090e5f8be3312ad1f1546
Vasquez <cvasquez857@gmail.com>
2024-06-01 23:01:30 -0600

    creacion de eliminar.php  asi mismo se agreego la ubicacion del archivo

--- controladores/clientes/buscar.php
+++ controladores/clientes/buscar.php
@@ -70,7 +70,7 @@ include_once '../../vistas/templates/header.php'; ?>
                                     </button>
                                     <ul class="dropdown-menu">
                                         <li><a class="dropdown-item" href="../../vistas/clientes/ModificarCliente.php?cli_codigo=<?= base64_encode($opcion['cli_codigo'])?>"><i class="bi bi-pencil-square me-2"></i>Modificar</a></li>
-                                        <li><a class="dropdown-item" href="#"><i class="bi bi-trash me-2"></i>Eliminar</a></li>
+                                        <li><a class="dropdown-item" href="../../vistas/clientes/eliminar.php?cli_codigo=<?= base64_encode($opcion['cli_codigo'])?>"><i class="bi bi-trash me-2"></i>Eliminar</a></li>
                                     </ul>
                                 </div>
                                 </td>
